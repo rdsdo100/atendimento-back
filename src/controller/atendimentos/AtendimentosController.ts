ace86331f673cdd9c49d1c95b22600b51a9ba93f
Rubens Diego <rdsdo2011@gmail.com>
2021-03-06 20:31:12 -0300

    Contrutinda atendimento

--- src/controller/atendimentos/AtendimentosController.ts
+++ src/controller/atendimentos/AtendimentosController.ts
@@ -1,13 +1,15 @@
-import {Request, Response} from "express";
-import {Controller, Get, Post} from "@overnightjs/core";
-import {Atendimentos} from "../../entity/Atendimentos";
+import { Request, Response } from "express";
+import { Controller, Get, Post } from "@overnightjs/core";
+import { Atendimentos } from "../../entity/Atendimentos";
 import AtendimentosBusiness from "../../business/atendimentos/AtendimentosBusiness";
+import { Empresas } from "../../entity/Empresas";
+import { Usuarios } from "../../entity/Usuarios";
 
 @Controller('atendimento')
-export default  class AtendimentosController {
+export default class AtendimentosController {
 
     @Get()
-    async buscarAtendimentoUsuarios(request: Request , response: Response) {
+    async buscarAtendimentoUsuarios(request: Request, response: Response) {
 
         const atendimentosBusiness = new AtendimentosBusiness()
         const retornoBuscaAtendimento = await atendimentosBusiness.buscarAtendimentosUsuarios(4)
@@ -17,18 +19,22 @@ export default  class AtendimentosController {
 
 
     @Post()
-    async cadastrarAtendimentos(request: Request , response: Response){
+    async cadastrarAtendimentos(request: Request, response: Response) {
         const atendimento = new Atendimentos()
-const atendimentosBusiness = new AtendimentosBusiness()
+        const empresa = new Empresas()
+        const usuario = new Usuarios()
+        const atendimentosBusiness = new AtendimentosBusiness()
 
         atendimento.descricaoAtendimento = String(request.body.descricaoAtendimento)
-        atendimento.usuariosIdFK.id = Number(1)
-        atendimento.empresasIdFK.id = Number(request.body.codigoEmpresaId)
+        empresa.id = Number(request.body.codigoEmpresaId)
+        usuario.id = Number(1)
         atendimento.dataAtendimento = new Date()
+        atendimento.usuariosIdFK = usuario
+        atendimento.empresasIdFK = empresa
+        console.log(atendimento)
 
+        const atendimentoSalvo = await atendimentosBusiness.cadastrarAtendimentos(atendimento)
 
-
-const atendimentoSalvo = await  atendimentosBusiness.cadastrarAtendimentos(atendimento)
-
+        return response.status(200).json(atendimentoSalvo)
     }
 }
