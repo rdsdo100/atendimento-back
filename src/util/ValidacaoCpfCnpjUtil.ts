7c5c5409172a957e88abab53584c4c12d6b3af79
Rubens Diego <rdsdo2011@gmail.com>
2021-05-03 13:08:22 -0300

    finalizando algoritimo de validação cpf e cnpj

--- src/util/ValidacaoCpfCnpjUtil.ts
+++ src/util/ValidacaoCpfCnpjUtil.ts
@@ -59,78 +59,91 @@ export default class ValidacaoCpfCnpjUtil {
         let dv2: number = 6
         let somatorioDv1Mod: number = 0
         let somatorioDv2Mod: number = 0
-        let dvFinal: String[]
-
-
-
+       
         for (let i: number = 0; i < cpfCnpjArray.length; i++) {
 
-
             if (i <= 11) {
-
                 somatorioDv1Mod = somatorioDv1Mod + (Number(cpfCnpjArray[i]) * dv1)
-
                 if (dv1 > 2) {
                     dv1 = dv1 - 1
                 } else {
                     dv1 = 9
                 }
             }
-
-            
             if (i <= 12) {
-             
                 somatorioDv2Mod = somatorioDv2Mod + (Number(cpfCnpjArray[i]) * dv2)
-
                 if (dv2 > 2) {
                     dv2 = dv2 - 1
                 } else {
                     dv2 = 9
                 }
             }
+       }
 
+        somatorioDv1Mod = 11 - (somatorioDv1Mod % 11)
+        somatorioDv2Mod = 11 - (somatorioDv2Mod % 11)
 
-
-        }
-
-
-
-        somatorioDv1Mod = 11- (somatorioDv1Mod%11) 
-        somatorioDv2Mod = 11- (somatorioDv2Mod%11) 
-
-        if(somatorioDv1Mod > 9){
+        if (somatorioDv1Mod > 9) {
             somatorioDv1Mod = 0
         }
-        if(somatorioDv2Mod > 9){
+        if (somatorioDv2Mod > 9) {
             somatorioDv2Mod = 0
         }
 
-
-
-
-
-console.log(somatorioDv1Mod , somatorioDv2Mod )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return "Cnpj"
+        if (somatorioDv1Mod === Number(cpfCnpjArray[12])) {
+            if (somatorioDv2Mod === Number(cpfCnpjArray[13])) {
+                return {validacao : true , tipo: "Cnpj" , somatorioDv1Mod , somatorioDv2Mod}
+            } else {
+                return {validacao : false , tipo: "Cnpj", somatorioDv1Mod , somatorioDv2Mod}
+            }
+        } else {
+            return {validacao : false , tipo: "Cnpj" ,somatorioDv1Mod , somatorioDv2Mod}
+        }
 
     }
 
     validarCpf(cpfCnpjArray: string[]) {
-        return "Cpf"
+        let dv1: number = 10
+        let dv2: number = 11
+        let somatorioDv1Mod: number = 0
+        let somatorioDv2Mod: number = 0
+       
+        for (let i: number = 0; i < cpfCnpjArray.length; i++) {
+
+          
+            if(i <= 8 ){
+                
+                somatorioDv1Mod = somatorioDv1Mod + (Number(cpfCnpjArray[i]) * dv1)
+                    dv1 = dv1 - 1
+            } 
+            
+            if(i <= 9){
+                somatorioDv2Mod = somatorioDv2Mod + (Number(cpfCnpjArray[i]) * dv2)
+                    dv2 = dv2 - 1
+                
+            }
+       }
+
+        somatorioDv1Mod = 11 - (somatorioDv1Mod % 11)
+        somatorioDv2Mod = 11 - (somatorioDv2Mod % 11)
+
+        if (somatorioDv1Mod > 9) {
+            somatorioDv1Mod = 0
+        }
+        if (somatorioDv2Mod > 9) {
+            somatorioDv2Mod = 0
+        }
+
+        if (somatorioDv1Mod === Number(cpfCnpjArray[9])) {
+            if (somatorioDv2Mod === Number(cpfCnpjArray[10])) {
+                return {validacao : true , tipo: "Cpf" , somatorioDv1Mod , somatorioDv2Mod}
+            } else {
+                return {validacao : false , tipo: "Cpf", somatorioDv1Mod , somatorioDv2Mod}
+            }
+        } else {
+            return {validacao : false , tipo: "Cpf" ,somatorioDv1Mod , somatorioDv2Mod}
+        }
+
     }
 
     validar(cpfCnpj: string) {
